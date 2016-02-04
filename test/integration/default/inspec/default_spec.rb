3de28b99a04ddb55074e923865595e9e54e95b62
Joe Gardiner <joe@grdnr.io>
2016-02-04 09:39:25 +0000

    Additional packages and tests

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -9,3 +9,15 @@ end
 describe package('unzip') do
   it { should be_installed }
 end
+
+describe package('libapache2-mod-wsgi') do
+  it { should be_installed }
+end
+
+describe package('python-pip') do
+  it { should be_installed }
+end
+
+describe package('python-mysqldb') do
+  it { should be_installed }
+end
