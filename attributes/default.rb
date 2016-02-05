b9a052be7190f1d630878c522a668fa94186f469
Joe Gardiner <joe@grdnr.io>
2016-02-05 09:28:04 +0000

    Ignored - contains insecure data. Temporary.

--- attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['path']['site'] = '/var/www/AAR'
-default['path']['conf'] = '/etc/apache2/sites-enabled'
-default['app']['source'] = 'https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
