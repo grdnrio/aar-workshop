7e3c1bd4a35f534fbff04eb8ad3729a01afb0311
Joe Gardiner <joe@grdnr.io>
2016-02-05 14:51:56 +0000

    No longer ignored by Git

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['firewall']['allow_ssh'] = true
+
+default['path']['site'] = '/var/www/AAR'
+default['path']['conf'] = '/etc/apache2/sites-enabled'
+
+default['app']['source'] = 'https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
