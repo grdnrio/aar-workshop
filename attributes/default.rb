bfd962172b7af2d071dfb2de3b9c34aa9c9d2faf
Joe Gardiner <joe@grdnr.io>
2016-02-05 08:48:58 +0000

    App and apache conf path attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['path']['site'] = '/var/www/AAR'
-default['path']['conf'] = '/etc/apache2/sites-enabled/'
+default['path']['conf'] = '/etc/apache2/sites-enabled'
+default['app']['source'] = 'https://github.com/colincam/Awesome-Appliance-Repair/archive/master.zip'
